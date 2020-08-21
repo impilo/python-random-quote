1a495345720b33e6761e7571c561c40b126ab574
Herman <User@DESKTOP-PRK4O68.hicnet.loc>
2020-08-21 07:33:17 +0200

    Write to file and show all quotes

--- get-quote.py
+++ get-quote.py
@@ -1,12 +1,19 @@
 def primary():
   #print("Keep it logically awesome.")
 
+  f = open("quotes.txt","a")
+  f.write("My new quote added here")
+  f.close();
+
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
 
-  print(quotes[0], end='')
-  print(quotes[14])
+  #print(quotes[0], end='')
+  #print(quotes[15])
+  
+  for x in quotes:
+      print(x, end='')
 
 if __name__== "__main__":
   primary()
